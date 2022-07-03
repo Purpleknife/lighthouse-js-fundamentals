dd21a0e57619edf633c0e5403888db1f3ccd62e3
Hind <hind.nayhi@gmail.com>
2022-07-03 15:01:45 -0600

    Used a While loop combined with an If statement to print the numbers from 100 to 200 with 3 exceptions: if the number is a multiple of 3, or 4, or both

---
+++ loopy-lighthouse-challenge.js
@@ -0,0 +1,13 @@
+let x = 100; //The starting point is 100.
+while (x <= 200) { //The condition is x<=200, the loop will stop at 200.
+  if (x % 3 === 0 && x % 4 === 0) { //If x is multiple of 3 and 4 both.
+    console.log('LoopyLighthouse');
+  } else if (x % 3 === 0) { //If x is a multiple of 3.
+    console.log('Loopy');
+  } else if (x % 4 === 0) { //If x is a multiple of 3.
+    console.log('Lighthouse'); 
+  } else {
+    console.log(x); //If none of the above, just print the number.
+  }
+  x++; //The loop gets incremented by 1 at each step.
+}

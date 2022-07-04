267c7419d74bf7f5bca9a38a14e46e9dd08f61fe
Hind <hind.nayhi@gmail.com>
2022-07-04 15:50:13 -0600

    Create a function to concatenate 2 arrays together using the concat() method

---
+++ concatenate-arrays-assignment.js
@@ -0,0 +1,9 @@
+//Create a function to concatenate 2 arrays together.
+
+const concat = function(array1,array2) {
+  return array1.concat(array2); //Used concat() method to merge the arrays.
+};
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);

4c73f0ac780e564d40f6e584842cd7a60eca9837
Hind <hind.nayhi@gmail.com>
2022-07-04 09:34:14 -0600

    Created a function to calculate the age based on the yearOfBirth and currentYear

---
+++ age-calculator-assignment.js
@@ -0,0 +1,7 @@
+//We want to calculate the age based on the yearOfBirth and currentYear.
+
+const ageCalculator = function (name,yearOfBirth,currentYear) {
+  return name+' is '+(currentYear-yearOfBirth)+' years old.';
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));

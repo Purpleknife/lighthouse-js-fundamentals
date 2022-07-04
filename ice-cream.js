7fab72e076b2e5d1af3f6474af487f0c9f791985
Hind <hind.nayhi@gmail.com>
2022-07-04 12:51:11 -0600

    Tinkering with arrays by adding items and using .length and index

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Add the 'root beer' flavor
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Log the first flavor
+console.log(iceCreamFlavours[0]);
+
+//Log the last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//Log the total number of flavors
+console.log(iceCreamFlavours.length);
